4a6b2e19373f6511839747abea5a3f9e7d2d8a88
wucuida <727438548@qq.com>
2016-10-10 16:31:05 +0800

    更新publicPath

--- static/index.js
+++ static/index.js
@@ -85,7 +85,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "static";
+/******/ 	__webpack_require__.p = "static/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
